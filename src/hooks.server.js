c855bf30050c5e3ecfd9ee0c4981835342211f1b
Anbcodes <me@anb.codes>
2025-02-01 18:53:47 -0500

    fixed shutdown hang issue

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,16 +1,31 @@
-import { db } from '$lib/db';
-import { redirect } from '@sveltejs/kit';
+import { db } from "$lib/db";
+import { redirect } from "@sveltejs/kit";
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-    const cookie = event.cookies.get('session');
-    const query = new URL(event.request.url).searchParams.get('s');
-    const session = query ?? cookie;
-	event.locals.session = (await db.query('SELECT * FROM sessions WHERE id = $1', [session])).rows[0];
+  const cookie = event.cookies.get("session");
+  const query = new URL(event.request.url).searchParams.get("s");
+  const session = query ?? cookie;
+  event.locals.session = (
+    await db.query("SELECT * FROM sessions WHERE id = $1", [session])
+  ).rows[0];
 
-	const response = await resolve(event, {
-        transformPageChunk: ({ html }) => html.replace('%theme%', event.cookies.get('theme') ?? '')
-    });
+  const response = await resolve(event, {
+    transformPageChunk: ({ html }) =>
+      html.replace("%theme%", event.cookies.get("theme") ?? ""),
+  });
 
-	return response;
+  return response;
 }
+
+export const init = async () => {
+  console.info("Server created, registering shutdown hooks");
+
+  process.on("sveltekit:shutdown", async (reason) => {
+    console.info("SvelteKit has shutdown because of", reason);
+
+    // Your custom logic for closing app specific resources
+    await db.end();
+    console.info("DB closed");
+  });
+};
