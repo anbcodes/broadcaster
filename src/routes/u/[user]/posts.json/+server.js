60187d0ff0eab9ba0b922597c51126787ac89fd6
Anbcodes <me@anb.codes>
2025-02-02 17:04:20 -0500

    removed ability to post from /posts

--- src/routes/u/[user]/posts.json/+server.js
+++ src/routes/u/[user]/posts.json/+server.js
@@ -1,26 +1,32 @@
 import { db } from "$lib/db.js";
-import { asyncFilter } from "$lib/util";
-import { checkViewable } from "$lib/util.server.js";
 import { json } from "@sveltejs/kit";
 
-export async function GET({ params, locals }) {
+export async function GET({ params, locals, fetch }) {
+  const loggedInUser = locals.session?.username;
+  const userForPosts = params.user;
+
+  /** @type {{name: string, owner: string}[]} */
+  const groups = await fetch(`/u/${loggedInUser}/groups.json`).then((r) =>
+    r.json()
+  );
+
   /** @type {import('pg').QueryResult<import('$lib/db.js').Post>} */
-  const posts = await db.query("SELECT * FROM posts WHERE username = $1", [
-    params.user,
-  ]);
+  const posts = await db.query(
+    "SELECT * FROM posts WHERE username = $1 AND ((($2) && posts.include AND NOT(($2) && posts.exclude)) OR username = $3) ORDER BY created DESC",
+    [
+      userForPosts,
+      [
+        "everyone",
+        `@${loggedInUser}`,
+        ...groups.map((group) => "#" + group.name),
+      ],
+      loggedInUser,
+    ]
+  );
 
   console.log(posts.rows);
 
-  const viewablePosts =
-    locals.session?.username !== params.user
-      ? await asyncFilter(posts.rows, (post) =>
-          checkViewable(
-            post.include,
-            post.exclude,
-            locals.session?.username ?? ""
-          )
-        )
-      : posts.rows;
+  const viewablePosts = posts.rows;
 
   return json(viewablePosts);
 }
